5ce78babd08331668b997d2cccb420ae2bbb8ad9
EllBor <elbor2@mail.ru>
2024-01-27 01:28:09 +0700

    added some media

--- app/js/main.js
+++ app/js/main.js
@@ -1,68 +1,28 @@
-
-  var problemList = document.querySelector('.problems__box-list');
-  var dropdownWord = document.querySelector('.problems__dropdown--word');
-  var dropdownSetting = document.querySelector('.problems__dropdown--setting');
-  var dropdownDesign = document.querySelector('.problems__dropdown--design');
-  var dropdownDocument = document.querySelector('.problems__dropdown--document');
-  var dropdownError = document.querySelector('.problems__dropdown--error');
+const chooseColor = document.querySelectorAll('.problems__list-item');
+const contentItem = document.querySelectorAll('.problems__dropdown');
 
 
-  problemList.addEventListener('click', function (event) {
-    var target = event.target;
-    if (target.classList.contains('problems__list-item--setting')) {
-        var activeElement = document.querySelector('.problems__list-item--active');
-        var activeDiv = document.querySelector('.problems__dropdown--active');
-        if (activeElement) {
-            activeElement.classList.remove('problems__list-item--active');
-        }
-        if (activeDiv) {
-            activeDiv.classList.remove('problems__dropdown--active');
-        }
-        dropdownSetting.classList.add('problems__dropdown--active');
-        target.classList.add('problems__list-item--active');
-    } else if (target.classList.contains('problems__list-item--design')) {
-        var activeElement = document.querySelector('.problems__list-item--active');
-        var activeDiv = document.querySelector('.problems__dropdown--active');
-        if (activeElement) {
-            activeElement.classList.remove('problems__list-item--active');
-        }
-        if (activeDiv) {
-            activeDiv.classList.remove('problems__dropdown--active');
-        }
-        dropdownDesign.classList.add('problems__dropdown--active');
-        target.classList.add('problems__list-item--active');
-    } else if (target.classList.contains('problems__list-item--word')) {
-        var activeElement = document.querySelector('.problems__list-item--active');
-        var activeDiv = document.querySelector('.problems__dropdown--active');
-        if (activeElement) {
-            activeElement.classList.remove('problems__list-item--active');
-        }
-        if (activeDiv) {
-            activeDiv.classList.remove('problems__dropdown--active');
-        }
-        dropdownWord.classList.add('problems__dropdown--active');
-        target.classList.add('problems__list-item--active');
-    }else if (target.classList.contains('problems__list-item--document')) {
-        var activeElement = document.querySelector('.problems__list-item--active');
-        var activeDiv = document.querySelector('.problems__dropdown--active');
-        if (activeElement) {
-            activeElement.classList.remove('problems__list-item--active');
-        }
-        if (activeDiv) {
-            activeDiv.classList.remove('problems__dropdown--active');
-        }
-        dropdownDocument.classList.add('problems__dropdown--active');
-        target.classList.add('problems__list-item--active');
-    }else if (target.classList.contains('problems__list-item--error')) {
-        var activeElement = document.querySelector('.problems__list-item--active');
-        var activeDiv = document.querySelector('.problems__dropdown--active');
-        if (activeElement) {
-            activeElement.classList.remove('problems__list-item--active');
-        }
-        if (activeDiv) {
-            activeDiv.classList.remove('problems__dropdown--active');
-        }
-        dropdownError.classList.add('problems__dropdown--active');
-        target.classList.add('problems__list-item--active');
-    }
-  });
+chooseColor.forEach(function(element){
+    element.addEventListener('click', open)
+})
+
+function open(evt){
+    const target = evt.currentTarget;
+    const button = target.classList[1];
+    const contentActive = document.querySelectorAll(`.${button}`);
+ 
+    chooseColor.forEach(function(item){
+        item.classList.remove('problems__list-item--active')
+    })
+
+    target.classList.add('problems__list-item--active')
+
+    contentItem.forEach(function(item){
+        item.classList.remove('problems__dropdown--active')
+    })
+
+    contentItem.forEach(function(item){
+        if (item.classList[1].replace('problems__dropdown--', '') === button.replace('problems__list-item--', ''))
+            item.classList.add('problems__dropdown--active')
+    })
+}
