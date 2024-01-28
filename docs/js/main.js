612ff268e8e347658feb88e2d479c749ef759944
EllBor <elbor2@mail.ru>
2024-01-28 18:16:35 +0700

    done

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,49 +1,57 @@
-const chooseColor = document.querySelectorAll('.problems__list-item');
-const contentItem = document.querySelectorAll('.problems__dropdown');
-const menuBtn = document.querySelectorAll('.header__btn');
-const menuRight = document.querySelectorAll('.rightside-menu');
-const menuClose = document.querySelectorAll('.rightside-menu__close');
+const chooseColor = document.querySelectorAll(".problems__list-item");
+const contentItem = document.querySelectorAll(".problems__dropdown");
+const menuBtn = document.querySelectorAll(".header__btn");
+const menuRight = document.querySelectorAll(".rightside-menu");
+const menuClose = document.querySelectorAll(".rightside-menu__close");
+const menuLink = document.querySelectorAll(".rightside-menu__list-link");
 
-chooseColor.forEach(function(element){
-    element.addEventListener('click', open)
-})
+chooseColor.forEach(function (element) {
+  element.addEventListener("click", open);
+});
 
-menuBtn.forEach(function(element){
-    element.addEventListener('click', menu)
-})
+menuBtn.forEach(function (element) {
+  element.addEventListener("click", menu);
+});
 
-menuClose.forEach(function(element){
-    element.addEventListener('click', close)
-})
+menuClose.forEach(function (element) {
+  element.addEventListener("click", close);
+});
 
-function open(evt){
-    const target = evt.currentTarget;
-    const button = target.classList[1];
- 
-    chooseColor.forEach(function(item){
-        item.classList.remove('problems__list-item--active')
-    })
+menuLink.forEach(function (element) {
+  element.addEventListener("click", close);
+});
 
-    target.classList.add('problems__list-item--active')
+function open(evt) {
+  const target = evt.currentTarget;
+  const button = target.classList[1];
 
-    contentItem.forEach(function(item){
-        item.classList.remove('problems__dropdown--active')
-    })
+  chooseColor.forEach(function (item) {
+    item.classList.remove("problems__list-item--active");
+  });
 
-    contentItem.forEach(function(item){
-        if (item.classList[1].replace('problems__dropdown--', '') === button.replace('problems__list-item--', ''))
-            item.classList.add('problems__dropdown--active')
-    })
+  target.classList.add("problems__list-item--active");
+
+  contentItem.forEach(function (item) {
+    item.classList.remove("problems__dropdown--active");
+  });
+
+  contentItem.forEach(function (item) {
+    if (
+      item.classList[1].replace("problems__dropdown--", "") ===
+      button.replace("problems__list-item--", "")
+    )
+      item.classList.add("problems__dropdown--active");
+  });
 }
 
-function menu(evt){
-    menuRight.forEach(function(item){
-        item.classList.remove('rightside-menu--close')
-    })
+function menu(evt) {
+  menuRight.forEach(function (item) {
+    item.classList.remove("rightside-menu--close");
+  });
 }
 
-function close(evt){
-    menuRight.forEach(function(item){
-        item.classList.add('rightside-menu--close')
-    })
+function close(evt) {
+  menuRight.forEach(function (item) {
+    item.classList.add("rightside-menu--close");
+  });
 }
